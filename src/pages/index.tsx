15b1069aaf471b5346d51af8dd1524f7a9db8bde
Juno <shy12@case.edu>
2023-02-09 10:36:11 +0900

    basic ui structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,16 @@
 import { type NextPage } from "next";
 const Home: NextPage = () => {
-  return <div className="">Hellow World</div>;
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
+      <div className="text-center text-2xl">Which Pokemon is Rounder?</div>
+      <div className="p-2"></div>
+      <div className="flex max-w-2xl items-center justify-between rounded border p-8">
+        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="p-8">Vs</div>
+        <div className="h-16 w-16 bg-red-200"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
